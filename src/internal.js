2cf3d6d438e7a7a86c2f00af61863e56f7cfc8d4
George Treviranus <3946669+geotrev@users.noreply.github.com>
2020-07-22 10:25:45 -0700

    Update internal.js

--- src/internal.js
+++ src/internal.js
@@ -2,9 +2,9 @@
 
 export const elementId = Symbol("#elementId")
 export const shadowRoot = Symbol("#shadowRoot")
-export const domMap = Symbol("#domMap")
+export const vDOM = Symbol("#vDOM")
 export const getDOMString = Symbol("#getDOMString")
-export const getDOMMap = Symbol("#getDOMMap")
+export const getVDOM = Symbol("#getVDOM")
 export const isFirstRender = Symbol("#isFirstRender")
 export const schedule = Symbol("#schedule")
 
