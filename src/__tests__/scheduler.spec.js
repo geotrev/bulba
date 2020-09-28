95fd036f46d8172062facd7f7354452763a5a708
George Treviranus <hello@geotrev.com>
2020-09-27 20:52:36 -0700

    add breakout comments in scheduler spec and remove unneeded empty mock

--- src/__tests__/scheduler.spec.js
+++ src/__tests__/scheduler.spec.js
@@ -4,27 +4,31 @@ import { getScheduler } from "../renderer/scheduler"
 console.info = jest.fn()
 const testFnFirst = () => console.info("nice")
 const testFnSecond = () => console.info("cool")
-
 const nextFrame = async () =>
   await new Promise((done) => requestAnimationFrame(done))
-
 const timeout = async () =>
   await new Promise((done) => setTimeout(done, 1000 / 60))
 
 describe("scheduler", () => {
   describe("requestAnimationFrame + cancelAnimationFrame", () => {
     it("schedules then runs the given function", async () => {
+      // Given
       const schedule = getScheduler()
+      // When
       schedule(testFnFirst)
       await nextFrame()
+      // Then
       expect(console.info).toBeCalledWith("nice")
     })
 
     it("cancels scheduled function if another is called before the end of frame", async () => {
+      // Given
       const schedule = getScheduler()
+      // When
       schedule(testFnFirst)
       schedule(testFnSecond)
       await nextFrame()
+      // Then
       expect(console.info).toBeCalledTimes(1)
       expect(console.info).toBeCalledWith("cool")
     })
@@ -36,17 +40,23 @@ describe("scheduler", () => {
     afterAll(() => (window.requestAnimationFrame = animationFrameFn))
 
     it("schedules then runs the given function", async () => {
+      // Given
       const schedule = getScheduler()
+      // When
       schedule(testFnFirst)
       await timeout()
+      // Then
       expect(console.info).toBeCalledWith("nice")
     })
 
     it("cancels scheduled function if another is called before the end of frame", async () => {
+      // Given
       const schedule = getScheduler()
+      // When
       schedule(testFnFirst)
       schedule(testFnSecond)
       await timeout()
+      // Then
       expect(console.info).toBeCalledTimes(1)
       expect(console.info).toBeCalledWith("cool")
     })
