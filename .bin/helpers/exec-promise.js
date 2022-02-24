6262dceb74242870ecb669395acd08c65d0202d3
George Treviranus <geowtrev@gmail.com>
2022-02-24 16:49:35 -0600

    chore: uptake release-workspaces to release all packages

--- .bin/helpers/exec-promise.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import { exec as e } from "child_process"
-import { promisify } from "util"
-
-const promisedExec = promisify(e)
-export const exec = async (cmd) => promisedExec(cmd, { stdio: "ignore" })
--- .bin/helpers/reporter.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import ora from "ora"
-
-export const reporter = ora()
-export const pkgReporter = ora({ indent: 4 })
